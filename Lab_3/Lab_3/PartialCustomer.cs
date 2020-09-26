1738c908f5df0e2c997b4db3663aa52c63a16766
Yuri Shust <shustts@yandex.by>
2020-09-26 16:52:29 +0300

    3-rd lab

---
+++ Lab_3/Lab_3/PartialCustomer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_3
+{
+    partial class Customer
+    {
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+            Customer b = obj as Customer;
+            if (b == null)
+                return false;
+            return this.Name == b.Name && this.Surname == b.Surname && this.FatherName == b.FatherName &&
+            this.balance == b.balance && this.CardNumber == b.CardNumber;
+        }
+
+        public override int GetHashCode()
+        {
+            return (CardNumber * 2)/3; 
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, Surname: {Surname}, Fathername: {FatherName}"; ;
+        }
+    }
+}
---
+++ Lab_3/Lab_3/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab_3
+{
+
+    class CustomerComparer : IComparer<Customer>
+    {
+        int IComparer<Customer>.Compare(Customer x, Customer y)
+        {
+            if (x.Name[0] > y.Name[0])
+                return 1;
+            else if (x.Name[0] < y.Name[0])
+                return -1;
+            else
+                return 0;
+        }
+    }
+
+    class Program
+    {
+
+        static void Main()
+        {
+            Customer a = new Customer("Yuri", "Shust", "Olegovich", 137, 19000);
+            a.PrintInfo();
+            a.AddToCard(1000);
+            a.PrintInfo();
+            a.MinusFromCard(2000);
+            a.PrintInfo();
+            Customer.TypeOfClass();
+            Customer b = new Customer("Jhordany", "-----", "Yovanovich", 1111, 100000);
+            Customer c = new Customer(a);
+            Console.WriteLine("---------------------------------");
+            if (a.Equals(b))
+            {
+                Console.WriteLine("Запись 1 = записи 2");
+            }
+            else
+                Console.WriteLine("Запись 1 != записи 2");
+
+            if (a.Equals(c))
+            {
+                Console.WriteLine("Запись 1 = записи 3");
+            }
+            else
+                Console.WriteLine("Запись 1 != записи 3");
+            Console.WriteLine("---------------------------------");
+
+            Customer[] arrCust = new Customer[3];
+            arrCust[0] = new Customer(a);
+            arrCust[1] = new Customer(b);
+            arrCust[2] = new Customer("Petr", "Hart", "Nikolaev", 654, 1000);
+
+            Array.Sort(arrCust, new CustomerComparer());
+
+            foreach (Customer element in arrCust)
+            {
+                element.PrintInfo();
+            }
+            Console.WriteLine("---------------------------------");
+
+            int bufPointLow, bufPointCup;
+            Console.WriteLine("Введите нижнюю границу диапазона");
+            bufPointLow = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите верхнюю границу диапазона");
+            bufPointCup = Convert.ToInt32(Console.ReadLine());
+
+            foreach (Customer element in arrCust)
+            {
+                if (element.CardNumber >= bufPointLow && element.CardNumber <= bufPointCup)
+                    element.PrintInfo();
+            }
+
+
+            Console.WriteLine("---------------------------------");
+            var User = new { Name = "Oleg", Age = 30 };
+            Console.WriteLine(User.Name + " " + User.Age);
+        }
+    }
+}
