91b1f64887581b7359da3cb23fb95411ed909da7
Yuri Shust <shustts@yandex.by>
2020-09-26 16:54:45 +0300

    4-th lab

---
+++ Lab_4/Lab_4/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Lab_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MySet set = new MySet(1, "SYO");
+            set.AddItem("Max");
+            set.AddItem("Nick");
+
+            MySet set2 = new MySet(2, "BEI");
+            set2.AddItem("Max");
+            set2.AddItem(" ");
+            set2.AddItem("Lia");
+            set2.AddItem("Nick");
+
+            MySet set3 = new MySet(3, "LIO");
+            set3.AddItem("Jio");
+            set3.AddItem("Mari");
+
+            MySet set4 = new MySet(4, "KPF");
+            set4.AddItem("Jhon");
+            set4.AddItem("");
+            set4.AddItem("Leo");
+
+            Console.WriteLine("--------- Перегрузка оператора - ---------");
+            string name = "Jio";
+            set3 = set3 - name;
+            set3.Show();
+            Console.WriteLine("--------- Перегрузка оператора * ------------");
+            set2 = set2 * set;
+            set2.Show();
+
+            Console.WriteLine("--------- Перегрузка оператора & ----------------");
+            set3 = set3 & set;
+            set3.Show();
+
+            Console.WriteLine("--------- Перегрузка оператора > ----------------");
+            Console.WriteLine(set > set2);
+
+            Console.WriteLine("--------- Перегрузка оператора < ----------------");
+            Console.WriteLine(set < set2);
+
+            Console.WriteLine("------------- Расширение Dif ----------------");
+            int r = set2.Dif();
+            Console.WriteLine(r);
+
+            Console.WriteLine("---------------------------------------------");
+
+            string abc = "awdg fseol  ajes";
+            Console.WriteLine($"Count of words: {abc.CountOfWords()}");
+            Console.WriteLine("---------------------------------------------");
+
+            int r2 = set3.Sum();
+            Console.WriteLine($"Sum lenght of all words: {r2}");
+            Console.WriteLine("---------------------------------------------");
+            set4.Show();
+            set4.RemoveSpace();
+            set4.Show();
+            Console.WriteLine("---------------------------------------------");
+            set4.CommaAfterWord();
+            set4.Show();
+        }
+    }
+}
---
+++ Lab_4/Lab_4/StatisticOperations.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_4
+{
+    static class StatisticOperations
+    {
+        public static int Max(this MySet set)
+        {
+            int len = set.GetItemByIndex(0).Length;
+            foreach (string item in set.GetHash())
+            {
+                if (len < item.Length)
+                    len = item.Length;
+            }
+            return len;
+        }
+
+        public static int Min(this MySet set)
+        {
+            int len = set.GetItemByIndex(0).Length;
+            foreach (string item in set.GetHash())
+            {
+                if (len > item.Length)
+                    len = item.Length ;
+            }
+            return len;
+        }
+
+        public static int Dif(this MySet set)
+        {
+            return Max(set) - Min(set);
+        }
+
+public static int CountOfWords(this string str)
+        {
+            int count = 0;
+            string[] words = str.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                count += words.Length;
+            return count;
+        }
+
+        public static int Sum(this MySet set)
+        {
+            int len = 0;
+            foreach(string item in set.GetHash())
+            {
+                len += item.Length;
+            }
+            return len;
+        }
+
+        public static void CommaAfterWord(this MySet set)
+        {
+            int len = set.GetSize();
+            HashSet<string> res = new HashSet<string>();
+            string buf = "";
+            for(int i = 0; i < len; i++)
+            {
+                buf = set.GetItemByIndex(i);
+                res.Add(buf);
+                res.Add(",");
+            }
+            //foreach (string item in res)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            set.collection = res;
+        }
+
+        public static void RemoveSpace(this MySet set)
+        {
+            set.collection.Remove("");
+        }
+    }
+}
