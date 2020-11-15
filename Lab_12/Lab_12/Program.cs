2874636fd8a563bc729e6d2f9d33f01ca5e1c61a
Yuri Shust <shustts@yandex.by>
2020-11-15 21:24:53 +0300

    upload

---
+++ Lab_12/Lab_12/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Lab_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Reflector reflector = new Reflector();
+            Reflector.ToFile(reflector.ToString());
+
+            Console.WriteLine("Methods: ");
+            foreach (var q in Reflector.GetMethods(reflector.ToString()))
+                Console.WriteLine("\t" + q.Name);
+
+
+            Console.WriteLine("Fields and properties: ");
+            foreach (var q in Reflector.GetFieldsProps(reflector.ToString()))
+                foreach (var qq in q)
+                    Console.WriteLine("\t" + qq.Name);
+
+
+            Console.WriteLine("Interfaces: ");
+            foreach (var q in Reflector.GetInterface(reflector.ToString()))
+                Console.WriteLine("\t" + q.Name);
+
+
+            Console.WriteLine();
+            TestClass main = new TestClass();
+            Reflector.PrintMet(main.ToString(), typeof(int));
+            Reflector.ToFile(main.ToString());
+            Reflector.ExecuteMethod(main.ToString(), "Hi");
+        }
+    }
+}
---
+++ Lab_12/Lab_12/TestClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_12
+{
+    class TestClass : ICloneable
+    {
+        public int num;
+        public string str;
+        public void Hi(string str, int num)
+        {
+            Console.WriteLine($"num: {num}, string: {str}");
+        }
+        
+        public object Clone()
+        {
+            return this.MemberwiseClone();
+        }
+    }
+}
